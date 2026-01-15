5901e36c569349c1136dbbca6d88ede75cafb3c4
WolfyOfPizza <84101762+andrewcarta@users.noreply.github.com>
2026-01-15 14:23:59 -0800

    Added headlights
    Only to shuttle ships

--- Assets/Scripts/InputManagerAdjuster.cs
+++ Assets/Scripts/InputManagerAdjuster.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class InputManagerAdjuster : MonoBehaviour
 {
     [SerializeField] private PlayerInputManager playerInputManager;
+    int activePlayers = 0;
+    int lastActivePlayers = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,13 @@ public class InputManagerAdjuster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        activePlayers = playerInputManager.playerCount;
+        if(activePlayers!=1 && activePlayers != lastActivePlayers)
+        {
+            
+        }
+        lastActivePlayers = activePlayers;
     }
+
+    
 }
